c73abde34e6f9c04a186b4f2ea1df92f4e02f8be
Vladislav <92388605+Radianser@users.noreply.github.com>
2023-04-26 13:46:01 +0400

    Update cart.php

--- cart.php
+++ cart.php
@@ -1,7 +1,7 @@
-<?php include 'connect.php' ?>
 <?php
     session_start();
     // session_destroy();
+    require_once 'connect.php';
     require_once 'functions.php';
     $data = json_decode(file_get_contents('php://input'), true);
 
